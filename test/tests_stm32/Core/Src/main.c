eb5d39ad4b5e6d643f7e31c639d6fa204c60dd9a
Aleksandr Vinogradov <vinogradov.alek@gmail.com>
2024-12-01 16:38:43 +0500

    configure freertos runtime stats for test project

--- test/tests_stm32/Core/Src/main.c
+++ test/tests_stm32/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "tim.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -89,6 +90,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
@@ -154,7 +156,6 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
-
 /* USER CODE END 4 */
 
 /**
